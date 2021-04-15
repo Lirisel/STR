9e39fbc751c5d2218006c459ebddad4ab8b20217
Lirisel <52199879+Lirisel@users.noreply.github.com>
2021-04-15 21:32:01 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,196 @@
+<? include('inc/header.php');?>
+<!--Первый блок мейна начало-->
+<div class="main-first-container">
+    <div class="wrapper">
+        <img src="img/logo/logo-main.png" alt="Логотип">
+        <div class="main-first-container-left-block">
+            <p>поставщик высокотехнологичного<br>
+                оборудования для переработки<br>
+                и производства полимерных изделий</p>
+            <p>один из крупнейших производителей<br>
+                вторичных гранул полипропилена<br>
+                в России</p>
+        </div>
+        <div class="main-first-container-center">
+            <p>Узнайте больше<br>
+                о нашей компании</p>
+            <a class="down-arrow js-scrollto" href="#about-us"></a>
+        </div>
+    </div>
+</div>
+<!--Первый блок мейна конец-->
+<!--О нас начало-->
+<div class="about-us" id="about-us">
+    <div class="wrapper">
+        <h2>О нашей компании</h2>
+        <div class="about-us-content">
+            <div class="about-us-content-left-block">
+                <p>В настоящее время, «СТР» занимает
+                    достойное место среди лидеров по
+                    переработке полимеров, производству
+                    полимерных композиционных
+                    материалов и поставке  качественного
+                    экструзионного оборудования из Китая
+                    на отечественный рынок.</p>
+            </div>
+            <div class="about-us-content-right-block">
+                <img src="img/images/about-us.jpg" width="728" height="270" alt="Оборудование производства">
+                <h3>Почему стоит выбрать<br>
+                    сотрудничество с "СТР":</h3>
+                <div>
+                    <p class="toggle">17 лет создаем и формируем рынок переработки полимеров</p>
+                    <div>
+                        <p>В настоящее время, «СТР» занимает
+                            достойное место среди лидеров по
+                            переработке полимеров, производству
+                            полимерных композиционных
+                            материалов и поставке  качественного
+                            экструзионного оборудования из Китая
+                            на отечественный рынок.</p>
+                    </div>
+                    <p class="toggle">Оборудование, которое "СТР" предлагает клиентам, проверено на практике</p>
+                    <div>
+                        <p>В настоящее время, «СТР» занимает
+                            достойное место среди лидеров по
+                            переработке полимеров, производству
+                            полимерных композиционных
+                            материалов и поставке  качественного
+                            экструзионного оборудования из Китая
+                            на отечественный рынок.</p>
+                    </div>
+                    <p class="toggle">Предприятие открытого типа</p>
+                    <div>
+                        <p>В настоящее время, «СТР» занимает
+                            достойное место среди лидеров по
+                            переработке полимеров, производству
+                            полимерных композиционных
+                            материалов и поставке  качественного
+                            экструзионного оборудования из Китая
+                            на отечественный рынок.</p>
+                    </div>
+                    <p class="toggle">Индивидуальный подход</p> <div>
+                        <p>В настоящее время, «СТР» занимает
+                            достойное место среди лидеров по
+                            переработке полимеров, производству
+                            полимерных композиционных
+                            материалов и поставке  качественного
+                            экструзионного оборудования из Китая
+                            на отечественный рынок.</p>
+                    </div>
+                    <p class="toggle">Забота об окружающей среде</p> <div>
+                        <p>В настоящее время, «СТР» занимает
+                            достойное место среди лидеров по
+                            переработке полимеров, производству
+                            полимерных композиционных
+                            материалов и поставке  качественного
+                            экструзионного оборудования из Китая
+                            на отечественный рынок.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--О нас конец-->
+<!--Выбор покупателей начало-->
+<div class="choice-of-buyers">
+    <div class="wrapper">
+        <h2>Выбор покупателей</h2>
+        <ul class="catalog-list">
+            <li class="catalog-item">
+                <a class="catalog-item-img yellow"><img src="img/produckts/str-25-lab.png" alt="Экструдер двухшнековый STR-25 lab"></a>
+                <a class="product-name">Экструдер двухшнековый STR-25 lab</a>
+                <p class="ruble-price">1 300 000 ₽</p>
+                <p class="dollar-price">15 300 $</p>
+                <a class="green-button" href="">Подробнее</a>
+            </li>
+            <li class="catalog-item">
+                <a class="catalog-item-img blue"><img src="img/produckts/str-36.png" alt="Экструдер двухшнековый STR-36"></a>
+                <a class="product-name">Экструдер двухшнековый STR-36</a>
+                <p class="ruble-price">1 300 000 ₽</p>
+                <p class="dollar-price">15 300 $</p>
+                <a class="green-button" href="">Подробнее</a>
+            </li>
+            <li class="catalog-item">
+                <a class="catalog-item-img green"><img src="img/produckts/str-25-lab.png" alt="Экструдер двухшнековый STR-25 lab"></a>
+                <a class="product-name">Экструдер двухшнековый STR-25 lab</a>
+                <p class="ruble-price">1 300 000 ₽</p>
+                <p class="dollar-price">15 300 $</p>
+                <a class="green-button" href="">Подробнее</a>
+            </li>
+            <li class="catalog-item">
+                <a class="catalog-item-img blue"><img src="img/produckts/str-36.png" alt="Экструдер двухшнековый STR-36"></a>
+                <a class="product-name">Экструдер двухшнековый STR-25 lab</a>
+                <p class="ruble-price">1 300 000 ₽</p>
+                <p class="dollar-price">15 300 $</p>
+                <a class="green-button" href="">Подробнее</a>
+            </li>
+            <li class="catalog-item">
+                <a class="catalog-item-img green"><img src="img/produckts/str-36.png" alt="Экструдер двухшнековый STR-36"></a>
+                <a class="product-name">Экструдер двухшнековый STR-36</a>
+                <p class="ruble-price">1 300 000 ₽</p>
+                <p class="dollar-price">15 300 $</p>
+                <a class="green-button" href="">Подробнее</a>
+            </li>
+            <li class="catalog-item">
+                <a class="catalog-item-img yellow"><img src="img/produckts/str-25-lab.png" alt="Экструдер двухшнековый STR-25 lab" ></a>
+                <a class="product-name">Экструдер двухшнековый STR-25 lab</a>
+                <p class="ruble-price">1 300 000 ₽</p>
+                <p class="dollar-price">15 300 $</p>
+                <a class="green-button" href="">Подробнее</a>
+            </li>
+        </ul>
+    </div>
+
+</div>
+<!--Выбор покупателей конец-->
+<!--Наши клиенты начало-->
+<div class="our-clients">
+    <div class="wrapper">
+        <h2>Наши клиенты</h2>
+        <div class="our-clients-item"><img src="img/logo/logo-disney.png" alt="Дисней" width="209" height="89"></div>
+        <div class="our-clients-item"><img src="img/logo/logo-fila.png" alt="Фила" width="195" height="67"></div>
+        <div class="our-clients-item"><img src="img/logo/logo-jaguar.png" alt="Ягуар" width="219" height="112"></div>
+        <div class="our-clients-item"><img src="img/logo/logo-adidas.png" alt="Адидас" width="136" height="133"></div>
+        <div class="our-clients-item"><img src="img/logo/logo-bp.png" alt="БП" width="205" height="129"></div>
+        <div class="our-clients-item"><img src="img/logo/logo-intel.png" alt="Интел" width="181" height="121"></div>
+        <div class="our-clients-item"><img src="img/logo/logo-gazprom.png" alt="Газпром" width="221" height="126"></div>
+        <div class="our-clients-item"><img src="img/logo/logo-canon.png" alt="Кэнон" width="224" height="47"></div>
+        <div class="our-clients-item"><img src="img/logo/logo-sony.png" alt="Сони" width="194" height="34"></div>
+        <div class="our-clients-item"><img src="img/logo/logo-cocacola.png" alt="Кока-кола" width="251" height="88"></div>
+    </div>
+</div>
+<!--Наши клиенты конец-->
+<!--Отзывы о нашей работе начало-->
+<div class="job-reviews">
+    <div class="wrapper">
+        <div class="job-reviews-top-block">
+            <h2>Отзывы<br>о нашей работе</h2>
+            <div class="navigation"></div>
+        </div>
+        <div class="owl-carousel owl-theme" id="review-slider">
+            <a class="slide gazprom" data-fancybox="reviews" data-src="img/images/review-gazprom.jpg">
+                <div class="slide-title">ПАО «Газпро́м</div>
+                <img class="slide-review" src="img/images/review-gazprom.jpg" alt="ПАО «Газпром́">
+                <img class="slide-logo" src="img/logo/logo-gazprom.png" alt="ПАО «Газпро́́">
+            </a>
+            <a class="slide coca-cola" data-fancybox="reviews" data-src="img/images/review-coca-cola.jpg">
+                <div class="slide-title">The Coca-Cola Company</div>
+                <img class="slide-review" src="img/images/review-coca-cola.jpg">
+                <img class="slide-logo" src="img/logo/logo-cocacola.png">
+            </a>
+            <a class="slide bp" data-fancybox="reviews" data-src="img/images/review-bp.jpg">
+                <div class="slide-title">bp global</div>
+                <img class="slide-review" src="img/images/review-bp.jpg" alt="">
+                <img class="slide-logo" src="img/logo/logo-bp.png" alt="">
+            </a>
+            <a class="slide coca-cola" data-fancybox="reviews" data-src="img/images/review-coca-cola.jpg">
+                <div class="slide-title">The Coca-Cola Company 2</div>
+                <img class="slide-review" src="img/images/review-coca-cola.jpg">
+                <img class="slide-logo" src="img/logo/logo-cocacola.png">
+            </a>
+        </div>
+    </div>
+</div>
+<!--Отзывы о нашей работе конец-->
+<? include('inc/footer.php');?>
